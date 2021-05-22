48b26dee1f06bea58f375d1ad785682a22eefbd0
Taras Yatsura <jatsura.taras@gmail.com>
2021-05-22 22:49:36 +0300

    JAR to WAR packaging: - main class (RestaurantVotingApplication) now implements SpringBootServletInitializer:   + Spring boot with using SpringServletContainerInitializer class autowire WebApplicationInitializer (using @HandlesTypes(WebApplicationInitializer.class) annotation):     ~ all classes which in classpath dir will be found and method on onStartup() will be put. - changes in pom.xml:   + packaging - war;   + new dependency spring-boot-starter-tomcat.
    NOTE: now app must be deployed to Tomcat

--- src/main/java/local/boot/spring/RestaurantVotingApplication.java
+++ src/main/java/local/boot/spring/RestaurantVotingApplication.java
@@ -2,9 +2,10 @@ package local.boot.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestaurantVotingApplication {
+public class RestaurantVotingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
