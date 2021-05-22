b661575384240eefe9bd9cffc86601ea044aed5c
Taras Yatsura <jatsura.taras@gmail.com>
2021-05-22 21:42:07 +0300

    Initial commit

---
+++ src/main/java/local/boot/spring/RestaurantVotingApplication.java
@@ -0,0 +1,13 @@
+package local.boot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+}
