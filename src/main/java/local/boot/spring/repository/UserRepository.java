7da4a892ad314b7c3abb6dea1f1fe57e97b8e0e9
Taras Yatsura <jatsura.taras@gmail.com>
2021-06-03 23:08:55 +0300

    Refactoring model: - User now extends BaseEntity instead AbstractPersistable<Integer>:   + BaseEntity implements Persistable<Integer>:     ~ implements isNew() and getId() methods.   + BaseEntity:     ~ MappedSuperclass;     ~ Hibernate works with it using fields;     ~ use Lombok for creating getters and setters, protected constructors without args and with all args, generating toString() method;     ~ has implementation of equals() and hashcode() methods;     ~ id now generate using IDENTITY strategy - delete id.new_generator_mappings: false property. - UserRepository now:   + has two additional request, that validate while repository creating - if it wrong app not start:     ~ one with using @Query - @Param now can be not used near method arg if compilation is run with parameter flag (in our case in spring-boot-starter-parent there is maven-compiler-plugin with parameters=true);     ~ one without it - dynamic request that is parsing by Spring Data JPA with using Hibernate criteria API.   + all request now only read only for Hibernate optimization;   + if have some modification methods - add @Transactional and @Modifying.

--- src/main/java/local/boot/spring/repository/UserRepository.java
+++ src/main/java/local/boot/spring/repository/UserRepository.java
@@ -2,5 +2,16 @@ package local.boot.spring.repository;
 
 import local.boot.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {}
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    List<User> findByLastNameContainingIgnoreCase(String lastName);
+}
